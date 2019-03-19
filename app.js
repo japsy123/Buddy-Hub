4cc6112d700b3d2ef48b84282641891148829cf1
Jal Panchal <jal08panchal@gmail.com>
2019-03-19 07:20:58 -0700

    Adding views with ejs

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ let express = require("express");
 let app = express();
 var path = require("path");
 
-
+app.set("view engine","ejs");
 app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname,"index.html"));
+    res.render("index");
 })
 
 app.listen(7000, (req,res) => {
